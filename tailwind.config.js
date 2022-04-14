a17a4151267b9638a0d35320e097ad09c11403b7
dandyash <dandyash0502@gmail.com>
2022-04-14 12:21:55 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        tomato: '#f64b3c',
+        cream: '#f6eedf'
+      },
+      fontFamily: {
+        Monteserrat: ['Montserrat'],
+        Archivo: ['Archivo'],
+        Poppins: ['Poppins'],
+        Nunito: ['Nunito'],
+        Calistoga: ['Calistoga']
+      }
+    },
+  },
+  plugins: [],
+}
